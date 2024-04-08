2bea2348d371c09770a3d2479423fa50e40ecc0f
joel-tjohn <joeltjohn10@gmail.com>
2024-04-09 04:58:41 +0530

    Add Bus Created

---
+++ src/Components/AddBus.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+
+
+const AddBus = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 cool-xxl-6">
+                                <label htmlFor="" className="form-label">Bus ID</label>
+                                <input type="text" className="form-control" />
+                            </div>
+
+
+
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 cool-xxl-6">
+
+                                <label htmlFor="" className="form-label">Number</label>
+                                <input className="form-control"></input>
+                            </div>
+
+
+
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 cool-xxl-12">
+
+                                <label htmlFor="" className="form-label">Route</label>
+                                <input className="form-control"></input>
+                            </div>
+
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 cool-xxl-6">
+
+                                <label htmlFor="" className="form-label">Driver Name</label>
+                                <input className="form-control"></input>
+                            </div>
+
+
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 cool-xxl-6">
+
+                                <label htmlFor="" className="form-label">Driver Number</label>
+                                <input className="form-control"></input>
+                            </div>
+
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 cool-xxl-6">
+
+                                <button className="btn btn-success">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddBus
