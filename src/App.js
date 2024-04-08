e499fadeb7331689028bec875faaea1335bafe9f
joel-tjohn <joeltjohn10@gmail.com>
2024-04-09 05:27:02 +0530

    View All Bus Created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AddBus from './Components/AddBus';
 import SearchBus from './Components/SearchBus';
 import DeleteBus from './Components/DeleteBus';
+import ViewAll from './Components/ViewAll';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <AddBus/>
     <SearchBus/>
     <DeleteBus/>
+    <ViewAll/>
     </div>
   );
 }
---
+++ src/Components/ViewAll.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const ViewAll = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <ul class="list-group">
+                                    <li class="list-group-item">SL NO</li>
+                                    <li class="list-group-item">1</li>
+                                    <li class="list-group-item">2</li>
+                                    <li class="list-group-item">3</li>
+                                    <li class="list-group-item">4</li>
+                                </ul>
+
+
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+
+                                <ul class="list-group">
+                                    <li class="list-group-item">BUS NUMBER</li>
+                                    <li class="list-group-item">KL 1 A 1234</li>
+                                    <li class="list-group-item">KL 17 Z 1404</li>
+                                    <li class="list-group-item">KL 36 J 7227</li>
+                                    <li class="list-group-item">KL B 23 3456</li>
+                                </ul>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                                <ul class="list-group">
+                                    <li class="list-group-item">ROUTE</li>
+                                    <li class="list-group-item">Trivandrum - Kochi</li>
+                                    <li class="list-group-item">Piravom - Ernakulam</li>
+                                    <li class="list-group-item">Thrissur - Munnar</li>
+                                    <li class="list-group-item">Pathanamthitta - Kollam</li>
+                                </ul>
+
+                            </div>
+                        
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ViewAll
