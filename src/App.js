3b51f324d08dec3503a3a8ca6c807dad5ebe19a5
joel-tjohn <joeltjohn10@gmail.com>
2024-04-09 05:02:14 +0530

    Search Bus Created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBus from './Components/AddBus';
+import SearchBus from './Components/SearchBus';
 
 function App() {
   return (
     <div className="App">
     
     <AddBus/>
+    <SearchBus/>
     </div>
   );
 }
