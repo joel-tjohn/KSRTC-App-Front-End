74ab7c855edc677782c8cdd5293d404004bf0e13
joel-tjohn <joeltjohn10@gmail.com>
2024-04-09 05:40:53 +0530

    Routing Applied and Project Completed

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import AddBus from './Components/AddBus';
 import SearchBus from './Components/SearchBus';
 import DeleteBus from './Components/DeleteBus';
 import ViewAll from './Components/ViewAll';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
     
-    <AddBus/>
-    <SearchBus/>
-    <DeleteBus/>
-    <ViewAll/>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<ViewAll/>}/>
+    <Route path='/m' element={<AddBus/>}/>
+    <Route path='/s' element={<SearchBus/>}/>
+    <Route path='/d' element={<DeleteBus/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
